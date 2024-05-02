8aad265f2a60f6cd5967dfba341ee494dde5e742
P PUNITH KRISHNA <32421081+punith300i@users.noreply.github.com>
2024-05-01 18:58:38 -0700

    Added Header and footers

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import dash_bootstrap_components as dbc
 import dash
+from dash import html
 import sys
 
 CERT_FILE = "./ssl/minmod_isi_edu_cert.cer"
@@ -9,8 +10,148 @@ FA = "https://use.fontawesome.com/releases/v5.8.1/css/all.css"
 
 app = dash.Dash(external_stylesheets=[dbc.themes.LITERA, FA], use_pages=True)
 
+header_layout = html.Div(
+    [
+        html.Img(
+            src="assets/favicon.png", style={"height": "30px", "marginRight": "10px"}
+        ),
+        html.Span(
+            "Min Mod",
+            style={"color": "black", "fontSize": "24px", "fontWeight": "bold"},
+        ),
+    ],
+    style={
+        "backgroundColor": "#ffffff",
+        "padding": "10px",
+        "display": "flex",
+        "alignItems": "center",
+    },
+)
+
+
+footer_layout = html.Footer(
+    [
+        dbc.Container(
+            [
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            [
+                                html.P(
+                                    [
+                                        "Data from: ",
+                                        html.A(
+                                            "https://minmod.isi.edu/sparql",
+                                            href="https://minmod.isi.edu/sparql",
+                                            style={
+                                                "color": "#D3D3D3",
+                                                "fontFamily": "Helvetica Neue, Helvetica, Arial, sans-serif",
+                                                "text-decoration": "none",
+                                            },
+                                        ),
+                                        html.Br(),
+                                        html.A(
+                                            "The data follows this schema (github)",
+                                            href="https://github.com/DARPA-CRITICALMAAS/schemas/tree/main/ta2",
+                                            target="_blank",
+                                            style={
+                                                "color": "#FDC93F",
+                                                "fontFamily": "Helvetica Neue, Helvetica, Arial, sans-serif",
+                                                "text-decoration": "none",
+                                            },
+                                        ),
+                                    ],
+                                    style={"color": "#D3D3D3"},
+                                ),
+                                html.Div(
+                                    [
+                                        html.P(
+                                            "Powered by",
+                                            style={"color": "#D3D3D3", "margin": "0px"},
+                                        ),
+                                        html.A(
+                                            html.Img(
+                                                src="./assets/lodview.png",
+                                                style={"height": "30px"},
+                                            ),
+                                            href="https://github.com/dvcama/LodView",
+                                        ),  # Adjust path as necessary
+                                    ],
+                                    style={"marginTop": "10px"},
+                                ),  # Container for LodView logo
+                            ],
+                            width=6,
+                            style={
+                                "display": "flex",
+                                "flexDirection": "column",
+                                "justifyContent": "center",
+                            },
+                        ),
+                        dbc.Col(
+                            [
+                                html.Div(
+                                    [
+                                        html.A(
+                                            "Center on Knowledge Graphs",
+                                            href="https://usc-isi-i2.github.io",
+                                            target="_blank",
+                                            style={
+                                                "color": "#D3D3D3",
+                                                "fontSize": "22px",
+                                                "fontFamily": "Helvetica Neue, Helvetica, Arial, sans-serif",
+                                                "text-decoration": "none",
+                                            },
+                                        ),
+                                        html.A(
+                                            [
+                                                html.Img(
+                                                    src="./assets/ISI_logo.png",
+                                                    style={
+                                                        "height": "60px",
+                                                        "marginTop": "10px",
+                                                    },
+                                                ),
+                                            ],
+                                            href="http://www.isi.edu",
+                                        ),
+                                    ],
+                                    style={
+                                        "display": "flex",
+                                        "flexDirection": "column",
+                                        "justifyContent": "center",
+                                        "alignItems": "flex-end",
+                                        "height": "100%",
+                                    },
+                                )
+                            ],
+                            width=5,
+                        ),
+                    ],
+                    className="g-0",
+                    style={
+                        "display": "flex",
+                        "alignItems": "center",
+                        "justifyContent": "space-between",
+                    },
+                )
+            ],
+            fluid=True,
+            style={
+                "backgroundColor": "#212121",
+                "padding": "20px",
+                "font-family": "Roboto, sans-serif;",
+            },
+        )
+    ]
+)
+
+
 app.layout = dash.html.Div(
     [
+        header_layout,
+        html.Hr(
+            style={"margin": "0px"},
+        ),
         dbc.Row(
             dbc.Nav(
                 [
@@ -51,6 +192,8 @@ app.layout = dash.html.Div(
             style={"margin": "0px"},
         ),
         dash.page_container,
+        html.Br(),
+        footer_layout,
     ],
 )
 
